b24da1aecb0356bd58fa397ce51fc519d51b4bde
qixingzhuiyue <1102959319@qq.com>
2017-07-13 18:08:10 +0800

    Update WechatController.class.php

--- Application/Home/Controller/WechatController.class.php
+++ Application/Home/Controller/WechatController.class.php
@@ -5,13 +5,9 @@ use Think\Controller;
 define("TOKEN", "weixin");
 
 class WechatController extends CommonController{
-	//wx6e144be882e1e0d0
-	//7b94f80642c8c78e9cfd5b51a6dc2b79
-	//public $appid = 'wxfa5a6521224f3471';
-	//public $secret = 'efa35b1a7cd6c35002d945a048dc04c7';
-	public $appid = 'wx604acb859c405539';
-    //public $secret = '1ce51548af788822c50fefa89e94d22a';
-	public $secret = '7056ef47d91f948e04bbf6c52e02043b';
+	public $appid = '';
+    //public $secret = '';
+	public $secret = '';
 //	public $redirect_uri = 'http://qifuzhiye.51ruanron.com/index.php/Home/Wechat/webAuth';
 
 	public function index(){
