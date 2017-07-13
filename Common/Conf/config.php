9fd82e8ec8b310d6ec722447d2768b8a43c604e8
qixingzhuiyue <1102959319@qq.com>
2017-07-13 17:22:42 +0800

    Update config.php

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -5,8 +5,6 @@ return array(
     'URL_MODEL'          => '1',    //URL模式
     'SESSION_AUTO_START' => 'true', //开启session
     'Wxreturn_url' => 'http://shop.51ruanron.com',
-//    'ORGANIZER' => '上海维沃珂自助营销平台',
-    //'Wxreturn_url' => 'http://shop.51ruanron.com',
 
     //数据库连接参数
     
@@ -15,19 +13,5 @@ return array(
     'DB_USER' => 'root',
     'DB_PWD' => '',
     'DB_NAME' => 'wework',
-    'DB_PREFIX' => 'ww_',
-    //数据库连接参数
-//
-//    'DB_TYPE' => 'mysql',
-//    'DB_HOST' => '139.224.27.198',
-//    'DB_USER' => 'wework',
-//    'DB_PWD' => 'ww123456',
-//    'DB_NAME' => 'wework',
-//    'DB_PREFIX' => 'ww_',
-//    'DB_TYPE' => 'mysql',
-//    'DB_HOST' => '106.14.65.213',
-//    'DB_USER' => 'wework',
-//    'DB_PWD' => 'asdfqwer!@#$1234',
-//    'DB_NAME' => 'wework',
-//    'DB_PREFIX' => 'ww_',
+    'DB_PREFIX' => 'ww_'
 );
