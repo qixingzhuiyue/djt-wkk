2a189ed84e45ddc27aabcf9393e6a851c8578254
djt <1102959319@qq.com>
2017-07-13 20:04:56 +0800

    eedit

--- Public/scripts/news.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017/2/22.
- */
-$(document).ready(function () {
-    // nav
-    $(".navbar-nav li:first-child").mouseout(function () {
-        $(this).addClass('listBg') ;
-    });
-})
